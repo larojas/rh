7a7ebf4701a4834dee26526ac918e6135beebbd9
Alberto Rojas <rojas.alberto@gmail.com>
2013-12-23 01:46:48 -0300

    Set to wpi-specific, and conform with Relay's interface.

--- relay_control.py
+++ relay_control.py
@@ -4,8 +4,8 @@
 # Copyright 2013 Alberto Rojas (rojas.alberto@gmail.com)
 # All rights reserved.
 
-import time
 import wiringpi2
+import relays
 
 from log import Log
 
@@ -15,41 +15,40 @@ PINMODE_OUTPUT = 1
 LEVEL_LO = 0
 LEVEL_HI = 1
 
-DELAY=0.5
+class WiringPiRelayControl(relays.Relays.RelayControl):
+  """Relay control using the WiringPi library."""
+  def __init__(self):
+    self._pins = set()
 
-_pins = set()
+  def init(self, pins):
+    wiringpi2.wiringPiSetup()
+    self._pins = set(pins)
+    Log('Setting pins %s to output mode...' % list(self._pins))
+    for i in self._pins:
+      wiringpi2.pinMode(i, PINMODE_OUTPUT)
+    Log("Done.")
 
-def Init(pins):
-  global _pins
-
-  wiringpi2.wiringPiSetup()
-  _pins = set(pins)
-  Log('Setting pins %s to output mode...' % list(_pins))
-  for i in _pins:
-    wiringpi2.pinMode(i, PINMODE_OUTPUT)
-  Log("Done.")
-
-def SampleLoop(pin):
-  Log("Beginning loop...")
-  for i in range(5):
-    On(pin)
-    time.sleep(DELAY)
-    Off(pin)
-    time.sleep(DELAY)
-  Log("Done.")
-
-def On(pin):
+  def close(self, pin):
   """Turn the relay at the specified pin on."""
-  if pin in _pins:
+  if pin in self._pins:
     wiringpi2.digitalWrite(pin, LEVEL_HI)
     return True
 
-def Off(pin):
-  """Turn the relay at the specified pin off."""
-  if pin in _pins:
-    wiringpi2.digitalWrite(pin, LEVEL_LO)
+  def open(self, pin):
+    """Turn the relay at the specified pin off."""
+    if pin in self._pins:
+      wiringpi2.digitalWrite(pin, LEVEL_LO)
 
-def Status(pin):
-  """Read the status of the specified pin."""
-  if pin in _pins:
-    return wiringpi2.digitalRead(pin)
+  def read(self, pin):
+    """Read the status of the specified pin."""
+    if pin in self._pins:
+      hwstatus = wiringpi2.digitalRead(pin)
+      if hwstatus == LEVEL_LO:
+        return rh.Relay.OPEN
+      elif hwstatus == LEVEL_HI:
+        return rh.Relay.CLOSED
+      return rh.Relay.UNKNOWN
+
+def get():
+  """To allow dynamic module load."""
+  return WiringPiRelayControl()
